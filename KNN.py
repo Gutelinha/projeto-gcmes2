55f1896e0cf2e99d0a85bd9d48996a46b50e685c
Gutelinha <gutolescura1@gmail.com>
2024-06-25 16:18:56 -0300

    warning de manutenibilidade resolvido part4 variavel local X

--- KNN.py
+++ KNN.py
@@ -12,23 +12,20 @@ class KNN:
         self.k = k
 
     def fit(self, x_data, y):
-        self.treino_x = x_data
-        self.treino_y = y
+        self._treino_x = x_data
+        self._treino_y = y
 
     def predict(self, x_data):
         previsoes = [self._predict(x) for x in x_data]
         return previsoes
 
-    
     def _predict(self, x):
-        #calcula distancia
-        dist = [distancia_euclidiana(x, treino_x) for treino_x in self.treino_x]
-
-        #pega k mais proximos
+        # Calcula a distância euclidiana
+        dist = [distancia_euclidiana(x, treino_x) for treino_x in self._treino_x]
+        # Pega os k mais próximos
         k_indices = np.argsort(dist)[:self.k]
-        k_classes_mais_proximas = [self.treino_y[i] for i in k_indices]
-
-        #classe majoritaria
+        k_classes_mais_proximas = [self._treino_y[i] for i in k_indices]
+        # Classe majoritária
         majoritaria = Counter(k_classes_mais_proximas).most_common()
         return majoritaria[0][0]
     
