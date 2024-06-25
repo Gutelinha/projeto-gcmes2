86013abd1820b8352f6e82ab6d7f305d1a3dc525
Gutelinha <gutolescura1@gmail.com>
2024-06-25 09:21:13 -0300

    Adicionar arquivos modificados e novos para teste e desenvolvimento

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
 import pytest
+from flask import url_for
+
 from main import app
+
 
 @pytest.fixture
 def client():
@@ -8,21 +11,24 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_home(client):
-    print("Running test: test_home")
-    response = client.get('/')
-    assert response.status_code == 302  # Redirect to /home
 
-def test_home_page(client):
-    print("Running test: test_home_page")
+def test_home_redirect(client):
+    response = client.get('/')
+    assert response.status_code == 302
+    assert response.headers['Location'] == 'http://localhost/home'
+
+
+def test_home(client):
     response = client.get('/home')
     assert response.status_code == 200
     assert b'Valor de K' in response.data
 
+
 def test_alterar_k(client):
     response = client.post('/alterar_k', data={'K': '5'})
-    assert response.status_code == 302  # Redirect after POST
+    assert response.status_code == 302
     assert b'Valor de K alterado para: 5' in response.data
+
 
 def test_testar_curriculo_ad(client):
     response = client.post('/testar_curriculo', data={
@@ -31,8 +37,9 @@ def test_testar_curriculo_ad(client):
         'Conexões': '100',
         'AD': 'on'
     })
-    assert response.status_code == 302  # Redirect after POST
-    assert 'A qualidade do currículo é' in response.data.decode('utf-8')
+    assert response.status_code == 302
+    assert 'A qualidade do currículo é' in response.get_data(as_text=True)
+
 
 def test_testar_curriculo_knn(client):
     response = client.post('/testar_curriculo', data={
@@ -40,17 +47,22 @@ def test_testar_curriculo_knn(client):
         'Publicações': '10',
         'Conexões': '100'
     })
-    assert response.status_code == 302  # Redirect after POST
-    assert 'A qualidade do currículo é' in response.data.decode('utf-8')
+    assert response.status_code == 302
+    assert 'A qualidade do currículo é' in response.get_data(as_text=True)
 
 def test_dados_treino(client):
     response = client.get('/dados_treino')
     assert response.status_code == 200
+    assert b'Valor de K' in response.data
+
 
 def test_dados_teste(client):
     response = client.get('/dados_teste')
     assert response.status_code == 200
+    assert b'Valor de K' in response.data
+
 
 def test_dados_teste_ad(client):
     response = client.get('/dados_teste_AD')
     assert response.status_code == 200
+    assert b'Valor de K' in response.data
