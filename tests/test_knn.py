cd0c441c10393b5bdd84a409e46dceafb1ac9739
Gutelinha <gutolescura1@gmail.com>
2024-06-25 09:06:58 -0300

    Adicionar arquivos modificados e novos para teste e desenvolvimento

---
+++ tests/test_knn.py
@@ -0,0 +1,37 @@
+import pytest
+import numpy as np
+from KNN import KNN, distancia_euclidiana
+
+def test_distancia_euclidiana():
+    print("Running test: test_distancia_euclidiana")
+    assert distancia_euclidiana(np.array([1, 2]), np.array([4, 6])) == 7
+
+@pytest.fixture
+def knn():
+    print("Running fixture: knn")
+    return KNN(k=3)
+
+def test_knn_fit(knn):
+    print("Running test: test_knn_fit")
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array(['Bom', 'Ruim', 'Bom'])
+    knn.fit(X, y)
+    assert (knn.Treino_X == X).all()
+    assert (knn.Treino_y == y).all()
+
+def test_knn_predict(knn):
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array(['Bom', 'Ruim', 'Bom'])
+    knn.fit(X, y)
+    pred = knn.predict(X)
+    assert len(pred) == len(X)
+
+def test_knn_set_previsao_precisao(knn):
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array(['Bom', 'Ruim', 'Bom'])
+    knn.fit(X, y)
+    knn.set_previsao_precisao(X, y)
+    assert hasattr(knn, 'previsoes')
+    assert hasattr(knn, 'precisao')
+    assert isinstance(knn.precisao, str)
+    assert 'Taxa de acertos' in knn.precisao
