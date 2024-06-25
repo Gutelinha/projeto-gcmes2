bda13d44d341393b4d1c2152e07f37a943c23094
Gutelinha <gutolescura1@gmail.com>
2024-06-25 10:31:26 -0300

    Corrigir services.py

--- Services.py
+++ Services.py
@@ -10,19 +10,28 @@ class tratamento_de_dados:
         return X
     
     def get_dados_gerais(self):
-        Pasta_Raiz = os.getcwd()
-        Pasta_Raiz = Pasta_Raiz + '\\dados'
-        arquivo = "Dados_Python.xlsx"
-        caminho = os.path.join(Pasta_Raiz,arquivo)
-        dados_gerais = pd.read_excel(caminho, sheet_name='Dados_Python')
-        dados_gerais = dados_gerais.drop(['Profissão'],axis=1)
-        dados_gerais = dados_gerais.dropna()
-        dados_gerais['Experiência'] = dados_gerais['Salário']/ dados_gerais['Experiência']
-        dados_gerais = dados_gerais.rename(columns={'Experiência':'Razão de Experiência'})
-        dados_gerais = dados_gerais.drop(['Salário'],axis=1)
+        try:
+            Pasta_Raiz = os.getcwd()
+            Pasta_Raiz = os.path.join(Pasta_Raiz, 'dados')
+            arquivo = "Dados_Python.xlsx"
+            caminho = os.path.join(Pasta_Raiz, arquivo)
 
-        return dados_gerais
-    
+            if not os.path.exists(caminho):
+                raise FileNotFoundError(f'O arquivo {arquivo} não foi encontrado no diretório {Pasta_Raiz}.')
+
+            dados_gerais = pd.read_excel(caminho, sheet_name='Dados_Python')
+            dados_gerais = dados_gerais.drop(['Profissão'], axis=1)
+            dados_gerais = dados_gerais.dropna()
+            dados_gerais['Experiência'] = dados_gerais['Salário'] / dados_gerais['Experiência']
+            dados_gerais = dados_gerais.rename(columns={'Experiência': 'Razão de Experiência'})
+            dados_gerais = dados_gerais.drop(['Salário'], axis=1)
+
+            return dados_gerais
+
+        except FileNotFoundError as e:
+            print(f'Erro: {e}')
+            return None
+        
     def get_x(self,dados_gerais):
         x = dados_gerais.loc[:,['Razão de Experiência', 'Publicações', 'Conexões']]
         return x
